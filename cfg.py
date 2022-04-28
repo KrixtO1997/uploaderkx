81cb3282b73f1c5af89df6aa5abbcddd69f479db
KrixTO1997 <89066325+KrixtO1997@users.noreply.github.com>
2022-04-28 23:16:30 +0200

    Add files via upload

---
+++ cfg.py
@@ -0,0 +1,6 @@
+# Token del Bot de BotFather
+TOKEN = '5097930764:AAEPWDQduOuIbMpKgbfRtUS9X9AfzHUlnR8'
+# API_ID de la cuenta de telegram
+API_ID = 7115785 
+# API_HASH de la cuenta de telegram
+API_HASH = '944a061fccf74d1500c617151de0d5cd'
